d91ce3b2c5d297d601b65e1bc5cf1476b3258d55
Tomek <tom@harvestflow.ai>
2026-01-24 13:11:42 +0100

    docs: update homepage documentation and assemble page
    - Create homepage.md as content source of truth
    - Assemble all components in Home.tsx
    - Finalize section ordering and layout

---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import { Hero } from '../components/home/Hero';
+import { Introduction } from '../components/home/Introduction';
+import { Philosophy } from '../components/home/Philosophy';
+import { Gallery } from '../components/home/Gallery';
+import { Experience } from '../components/home/Experience';
+import { Concept } from '../components/home/Concept';
+import { Story } from '../components/home/Story';
+import { Location } from '../components/home/Location';
+import { Testimonials } from '../components/home/Testimonials';
+
+export const Home = () => {
+  return (
+    <div className="flex flex-col w-full">
+      <Hero />
+      <Introduction />
+      <Gallery />
+      <Testimonials />
+      <Philosophy />
+      <Experience />
+      <Concept />
+      <Story />
+      <Location />
+    </div>
+  );
+};
